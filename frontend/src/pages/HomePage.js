da52b7f295ba6b8539c6595bce9461f02c4a3e2c
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-31 02:44:29 +0530

    people added

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-// const people = [
-//   {
-//     name: "John Doe",
-//     title: "Front-end Developer",
-//     department: "Engineering",
-//     email: "john@devui.com",
-//     role: "Developer",
-//     image:
-//       "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
-//   },
-//   {
-//     name: "Jane Doe",
-//     title: "Back-end Developer",
-//     department: "Engineering",
-//     email: "jane@devui.com",
-//     role: "CTO",
-//     image:
-//       "https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
-//   },
-// ];
+const people = [
+  {
+    name: "John Doe",
+    title: "Front-end Developer",
+    department: "Engineering",
+    email: "john@devui.com",
+    role: "Developer",
+    image:
+      "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
+  },
+  {
+    name: "Jane Doe",
+    title: "Back-end Developer",
+    department: "Engineering",
+    email: "jane@devui.com",
+    role: "CTO",
+    image:
+      "https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+  },
+];
 
 const HomePage = () => {
   const [empData, setEmpData] = useState();
